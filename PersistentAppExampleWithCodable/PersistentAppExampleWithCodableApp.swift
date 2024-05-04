f3f228077407e41aae5417c69aa0ba39a7751dfd
Ricardo Venieris <rvenieris@gmail.com>
2024-05-04 20:06:16 -0300

    Initial Commit

---
+++ PersistentAppExampleWithCodable/PersistentAppExampleWithCodableApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersistentAppExampleWithCodableApp.swift
+//  PersistentAppExampleWithCodable
+//
+//  Created by Ricardo Almeida Venieris on 04/05/24.
+//
+
+import SwiftUI
+
+@main
+struct PersistentAppExampleWithCodableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
